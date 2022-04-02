7d8367e5f2a9e0481b82f07f923baa457de7ccaa
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-02 07:30:54 -0300

    Fix: Auth-Google

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -7,11 +7,20 @@ import { fetchUser } from '../utils/fetchUser';
 
 import styles from './styles.module.css';
 
-const Home = () => {
-  const navigate = useNavigate();
+const userInfo = fetchUser();
 
-  const user = fetchUser();
+const Home = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
   console.log(user);
+
+  if (!user) {
+    navigate('/login');
+  }
+
+  useEffect(() => {
+    setUser(userInfo);
+  }, [userInfo]);
 
   const logout = () => {
     localStorage.clear();
@@ -20,34 +29,40 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div>
-        <img
-          className={styles.banner}
-          src="https://source.unsplash.com/1600x900/?nature,photography,technology"
-          alt="banner-pic"
-        />
-      </div>
-      <div>
-        <h1>Olá, {user.name}</h1>
-        <h1>E-mail, {user.email}</h1>
-        <img classname={styles.photo} src={user.imageUrl} alt="user-pic" />
-        <GoogleLogout
-          clientId="90248417657-u6t72ervqateh93ruk93uq54v7rfimaj.apps.googleusercontent.com"
-          render={(renderProps) => (
-            <button
-              type="button"
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-            >
-              <AiOutlineLogout color="red" fontSize={28} />
-            </button>
-          )}
-          onLogoutSuccess={logout}
-          cookiePolicy="single_host_origin"
-        />
-      </div>
-    </div>
+    <>
+      {user ? (
+        <div className={styles.container}>
+          <div>
+            <img
+              className={styles.banner}
+              src="https://source.unsplash.com/1600x900/?nature,photography,technology"
+              alt="banner-pic"
+            />
+          </div>
+          <div>
+            <h1>Olá, {user.name}</h1>
+            <h1>E-mail: {user.email}</h1>
+            <img classname={styles.photo} src={user.imageUrl} alt="user-pic" />
+            <GoogleLogout
+              clientId="90248417657-u6t72ervqateh93ruk93uq54v7rfimaj.apps.googleusercontent.com"
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <AiOutlineLogout color="red" fontSize={28} />
+                </button>
+              )}
+              onLogoutSuccess={logout}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
+        </div>
+      ) : (
+        <h1>Não consegui recuperar suas credenciais</h1>
+      )}
+    </>
   );
 };
 
